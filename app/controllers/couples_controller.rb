e5b51158355e2a09efc761993c33b9f7a7ba237b
Nicoara <nicoaratalpes@gmail.com>
2018-05-09 16:32:42 +0300

    feature - Couple index, new, edit with dancer names

--- app/controllers/couples_controller.rb
+++ app/controllers/couples_controller.rb
@@ -13,10 +13,14 @@ class CouplesController < ApplicationController
 
   def new
     @couple = Couple.new
+    @lstBoys = Dancer.where(is_boy: true)
+    @lstGirls = Dancer.where(is_boy: false)
   end
 
   # GET /couples/1/edit
   def edit
+    @lstBoys = Dancer.where(is_boy: true)
+    @lstGirls = Dancer.where(is_boy: false)
   end
 
   def create
