4b1904ba0196ba3edf2a59b70b8605af939e77cd
Nicoara <nicoaratalpes@gmail.com>
2018-05-10 12:02:15 +0300

    update - update variable names in Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 countries = ['Germany', 'Italy', 'Poland', 'Austria', 'Romania', 'Russia', 'Japan'];
-lstCurrencies = ['USD', 'EUR', 'RON', 'YEN', 'SEK']
+lst_currencies = ['USD', 'EUR', 'RON', 'YEN', 'SEK']
 prng = Random.new
-nrPairs = 20
+nr_couples = 100
 
 
 Agesection.create!(name: 'Adult')
@@ -21,47 +21,44 @@ Agesection.create!(name: 'Senior III')
 Division.create!(name: 'General')
 Division.create!(name: 'Professional')
 
-nrPairs.times do |n|
-  randomCountry = prng.rand(countries.length)
+nr_couples.times do |n|
+  country_id = prng.rand(countries.length)
   name = Faker::Name.unique.name
-  randomGender = prng.rand(2)
-  gender = true
-  if (randomGender == 0)
-    gender = false
-  end
-  Dancer.create!(name: name, country: countries[randomCountry], is_boy: gender)
+  gender = prng.rand(2) == 0 ? true : false
+  Dancer.create!(name: name, country: countries[country_id], is_boy: gender)
 end
 
-nrPairs.times do |n|
-  randomBoy = prng.rand(nrPairs)
-  randomGirl = prng.rand(nrPairs)
-  Couple.create(boy_id: randomBoy, girl_id: randomGirl)
+nr_couples.times do |n|
+  boy_id = prng.rand(nr_couples)
+  girl_id = prng.rand(nr_couples)
+  Couple.create(boy_id: boy_id, girl_id: girl_id)
 end
 
 20.times do |n|
-  randomName = Faker::Lorem.word + " Competition"
-  randomDate = Date.today+rand(365)
-  randomCountry = prng.rand(countries.length)
-  randomPrice = prng.rand(500)
-  randomCurrencyIndex = prng.rand(lstCurrencies.length)
-  randomMaxCouples = 6 + prng.rand(6)
+  name = Faker::Lorem.word + " Competition"
+  date = Date.today + rand(365)
+  country_id = prng.rand(countries.length)
+  price = prng.rand(500)
+  currency_index = prng.rand(lst_currencies.length)
+  max_couples = 6 + prng.rand(6)
 
-  Competition.create(name: randomName,
-    date: randomDate,
-    country: countries[randomCountry],
-    price: randomPrice,
-    currency: lstCurrencies[randomCurrencyIndex],
-    maxCouplesOnFloor: randomMaxCouples)
+  Competition.create(name: name,
+    date: date,
+    country: countries[country_id],
+    price: price,
+    currency: lst_currencies[currency_index],
+    maxCouplesOnFloor: max_couples)
 end
 
-nrPairs.times do |n|
-  randomCouple = prng.rand(Couple.all.length)
-  randomCompetition = prng.rand(Competition.all.length)
-  randomDivision = prng.rand(Division.all.length)
-  randomAgesection = prng.rand(Agesection.all.length)
+100.times do |n|
 
-  Entry.create(couple_id: randomCouple, competition_id: randomCompetition,
-    division_id: randomDivision, agesection_id: randomAgesection)
+  couple_id = prng.rand(Couple.count)
+  competition_id = prng.rand(Competition.count)
+  division_id = prng.rand(Division.count)
+  agesection_id = prng.rand(Agesection.count)
+
+  Entry.create(couple_id: couple_id, competition_id: competition_id,
+    division_id: division_id, agesection_id: agesection_id)
 end
 
 
