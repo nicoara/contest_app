4b8f8d3f8bd897aa264dcc8327ca8aed51e0a767
Nicoara <nicoaratalpes@gmail.com>
2018-05-14 16:21:18 +0300

    bugfix - Couples controller, added test new, bugfix that works on empty db

--- test/controllers/couples_controller_test.rb
+++ test/controllers/couples_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class CouplesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @couple_test = couples(:BenedettoAndClaudia)
+  end
+
   test "should get index" do
     get couples_url
     assert_response :success
@@ -10,4 +14,5 @@ class CouplesControllerTest < ActionDispatch::IntegrationTest
     get new_couple_url
     assert_response :success
   end
+
 end
