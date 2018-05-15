a8858681e2718e0b2c6677d3f239707f867fc09b
Nicoara <nicoaratalpes@gmail.com>
2018-05-15 13:05:07 +0300

    feature - tests for query with couples from different countries

--- test/controllers/data_controller_test.rb
+++ test/controllers/data_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class DataControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    boy_germany = create(:dancer, name: 'Jurgen Klopp', country: 'Germany', is_boy: true)
+    girl_germany = create(:dancer, name: 'Angela Merkel', country: 'Germany', is_boy: false)
+    girl_italy = create(:dancer, name: 'Alessandra Elati', country: 'Italy', is_boy: false)
+    @couple_italy = create(:couple, boy_id: boy_germany.id, girl_id: girl_italy.id)
+    @couple_germany = create(:couple, boy_id: boy_germany.id, girl_id: girl_germany.id)
+  end
+
+  test "should show correct couple on query couple diff countries" do
+    get couples_diff_countries_url
+    assert_response :success
+    text = "id: #{@couple_italy.id}"
+    assert_select 'p', text
+  end
+
+  test "should not show incorrect couple on query couple diff countries" do
+    get couples_diff_countries_url
+    assert_response :success
+    text = "id: #{@couple_germany.id}"
+    assert_select "p", text: text, count: 0
+  end
 end
