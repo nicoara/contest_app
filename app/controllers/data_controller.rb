0b5c53856ac74b626ee4da50fc9580160a03bde2
Nicoara <nicoaratalpes@gmail.com>
2018-05-16 13:11:17 +0300

    update - rewrite query for dancers from different countries to be fast

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -6,13 +6,26 @@ class DataController < ApplicationController
     @dancer_with_most_competitions_title = 'Dancer with most competitions'
   end
 
-  def couples_diff_countries
+   def couples_diff_countries
     @title = 'Couples with dancers from different countries'
+
+    lst_countries_ids = {}
+    lst_countries = Dancer.pluck(:country).uniq
+    lst_countries.each do |country|
+      lst_countries_ids[country] = Dancer.where(country: country).pluck(:id)
+    end
 
     @lst_couples = []
     Couple.find_each do |couple|
-      if Dancer.find(couple.boy_id).country != Dancer.find(couple.girl_id).country
-        @lst_couples.push(couple)
+      lst_countries_ids.each do |country, country_ids|
+        is_country_boy = country_ids.include? couple.boy_id
+        is_country_girl = country_ids.include? couple.girl_id
+        if is_country_boy or is_country_girl
+          if is_country_boy == false || is_country_girl == false
+            @lst_couples.push(couple)
+          end
+          break
+        end
       end
     end
 
