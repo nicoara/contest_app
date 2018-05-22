3e089930c27d31f4e67f89ed4e3e393771efcec9
Nicoara <nicoaratalpes@gmail.com>
2018-05-22 10:52:30 +0300

    update - remove title variable from controller

--- app/controllers/international_couples_controller.rb
+++ app/controllers/international_couples_controller.rb
@@ -1,8 +1,5 @@
 class InternationalCouplesController < ApplicationController
   def index
-    @title = 'Couples with dancers from different countries'
-
-
     @lst_couples = Couple.select(:id)
       .joins("LEFT JOIN dancers as d ON couples.boy_id = d.id")
       .joins("LEFT JOIN dancers as d1 ON couples.girl_id = d1.id")
