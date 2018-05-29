269ddb330d49985524499dd04ed97cd67b9f86b7
Nicoara <nicoaratalpes@gmail.com>
2018-05-29 15:31:13 +0300

    Rename dancers_most_age_sections_test variables that don't have relevance to domain

--- test/controllers/dancers_most_age_sections_controller_test.rb
+++ test/controllers/dancers_most_age_sections_controller_test.rb
@@ -2,33 +2,33 @@ require 'test_helper'
 
 class DancersMostAgeSectionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    @girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    girl_italy = create(:girl, name: 'Alessandra Elati', country: 'Italy')
-    couple_diff_country = create(:couple, boy_id: @boy_germany.id, girl_id: girl_italy.id)
-    couple_same_country = create(:couple, boy_id: @boy_germany.id, girl_id: @girl_germany.id)
+    @boy = create(:boy)
+    @girl = create(:girl)
+    girl_2 = create(:girl)
+    couple = create(:couple, boy_id: @boy.id, girl_id: girl_2.id)
+    couple_2 = create(:couple, boy_id: @boy.id, girl_id: @girl.id)
 
     competition = create(:competition)
     division = create(:division)
     age_section = create(:age_section)
     age_section_2 = create(:age_section)
-    entry = create(:entry, couple_id: couple_diff_country.id, competition_id: competition.id,
+    entry = create(:entry, couple_id: couple.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id)
-    entry_2 = create(:entry, couple_id: couple_same_country.id, competition_id: competition.id,
+    entry_2 = create(:entry, couple_id: couple_2.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section_2.id)
   end
 
   test "should show correct dancer with most age sections" do
     get dancers_most_age_sections_url
     assert_response :success
-    text = "id: #{@boy_germany.id}"
+    text = "id: #{@boy.id}"
     assert_select 'p', text: text, count: 1
   end
 
   test "should not show incorrect dancer with most age sections" do
     get dancers_most_age_sections_url
     assert_response :success
-    text = "id: #{@girl_germany.id}"
+    text = "id: #{@girl.id}"
     assert_select 'p', text: text , count: 0
   end
 end
