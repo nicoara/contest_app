ad3acb12e8285fbe1ddcc2c3e18f3fc4bcf6a056
Nicoara <nicoaratalpes@gmail.com>
2018-05-29 14:29:40 +0300

    bugfix - dancer_partners works for girls, add test

--- app/models/dancer_partners.rb
+++ app/models/dancer_partners.rb
@@ -1,7 +1,7 @@
 class DancerPartners
 
   def self.of(dancer_id)
-    is_boy = Dancer.select('is_boy').find(dancer_id)
+    is_boy = Dancer.find(dancer_id).is_boy
     partner_ids = is_boy ?
       Couple.where(boy_id: dancer_id).select(:girl_id) :
       Couple.where(girl_id: dancer_id).select(:boy_id)
---
+++ test/controllers/dancer_partners_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class DancerPartnersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @boy = create(:boy, name: 'Jurgen Klopp')
+    @boy_2 = create(:boy)
+    @girl = create(:girl)
+    @girl_2 = create(:girl)
+    create(:couple, boy_id: @boy.id, girl_id: @girl.id)
+    create(:couple, boy_id: @boy_2.id, girl_id: @girl.id)
+    create(:couple, boy_id: @boy.id, girl_id: @girl_2.id)
+  end
+
+  test "should show partners for a girl" do
+    get dancer_partner_url(@girl.id)
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@boy.id.to_s+"']"
+    assert_select "li[id='"+@boy_2.id.to_s+"']"
+  end
+
+  test "should show partners for a boy" do
+    get dancer_partner_url(@boy.id)
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@girl.id.to_s+"']"
+    assert_select "li[id='"+@girl_2.id.to_s+"']"
+  end
+end
