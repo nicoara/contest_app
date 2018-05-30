e49399c4dc002c430cedc7aeb8fb99ebec5b1975
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 13:06:37 +0300

    Add dancer names in international_couples query

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -1,11 +1,17 @@
 class DancersController < ApplicationController
+  before_action :set_dancer, only: [:show]
+
   def index
     @dancers = Dancer.all
+  end
+
+  def show
   end
 
   def new
     @dancer = Dancer.new
   end
+
 
   def create
     @dancer = Dancer.new(dancer_params)
@@ -17,6 +23,9 @@ class DancersController < ApplicationController
   end
 
   private
+    def set_dancer
+      @dancer = Dancer.find(params[:id])
+    end
 
     def dancer_params
       params.require(:dancer).permit(:name, :country, :is_boy)
--- app/controllers/international_couples_controller.rb
+++ app/controllers/international_couples_controller.rb
@@ -4,6 +4,6 @@ class InternationalCouplesController < ApplicationController
       .joins("LEFT JOIN dancers as d ON couples.boy_id = d.id")
       .joins("LEFT JOIN dancers as d1 ON couples.girl_id = d1.id")
       .where("d.country != d1.country")
-      .select(:id)
+      .select('couples.id as id, d.name as boy_name, d.id as boy_id, d1.name as girl_name, d1.id as girl_id')
   end
 end
