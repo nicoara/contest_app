0691df5814e487ef8adb7c535f30007ece862480
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 16:58:22 +0300

    Improve performance competition_most_earnings query, added multiple solutions

--- app/controllers/competition_most_earnings_controller.rb
+++ app/controllers/competition_most_earnings_controller.rb
@@ -1,15 +1,20 @@
 class CompetitionMostEarningsController < ApplicationController
   def index
-    competition_most_earnings = Entry
+    most_earnings = Entry
       .joins(:competition)
       .group('entries.competition_id')
       .order('earnings DESC')
-      .select('entries.competition_id as id, sum(competitions.price) as earnings')
+      .select('sum(competitions.price) as earnings')
       .first
 
-    if competition_most_earnings != nil
-      @competition_id = competition_most_earnings.id
-      @earnings = competition_most_earnings.earnings
+    if most_earnings != nil
+      @earnings = most_earnings.earnings
+
+      @competitions_most_earnings = Entry
+        .joins(:competition)
+        .group('entries.competition_id')
+        .having('sum(competitions.price) == ' + most_earnings.earnings.to_s)
+        .select('competitions.id as id, competitions.name as name')
     end
   end
 end
