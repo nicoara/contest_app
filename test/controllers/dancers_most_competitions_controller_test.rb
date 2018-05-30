43fe2030e2ab4b11eb4d9e254e55424f362d952a
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 10:56:12 +0300

    Add test for multiple solutions for dancers_most_competitions query

--- test/controllers/dancers_most_competitions_controller_test.rb
+++ test/controllers/dancers_most_competitions_controller_test.rb
@@ -2,31 +2,35 @@ require 'test_helper'
 
 class DancersMostCompetitionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy = create(:boy)
+    @boy_most_competitions = create(:boy)
+    @girl_most_competitions = create(:girl)
     @girl = create(:girl)
-    girl_2 = create(:girl)
-    couple = create(:couple, boy_id: @boy.id, girl_id: girl_2.id)
-    couple_2 = create(:couple, boy_id: @boy.id, girl_id: @girl.id)
+    boy_2 = create(:boy)
+    couple = create(:couple, boy_id: @boy_most_competitions.id, girl_id: @girl_most_competitions.id)
+    couple_2 = create(:couple, boy_id: @boy_most_competitions.id, girl_id: @girl.id)
+    couple_3 = create(:couple, boy_id: boy_2.id, girl_id: @girl_most_competitions.id)
     competition = create(:competition)
     division = create(:division)
     age_section = create(:age_section)
-    entry = create(:entry, couple_id: couple.id, competition_id: competition.id,
+    create(:entry, couple_id: couple.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id)
-    entry_2 = create(:entry, couple_id: couple_2.id, competition_id: competition.id,
+    create(:entry, couple_id: couple_2.id, competition_id: competition.id,
+                      division_id: division.id, age_section_id: age_section.id)
+    create(:entry, couple_id: couple_3.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id)
   end
 
   test "should show correct dancer with most competitions" do
     get dancers_most_competitions_url
     assert_response :success
-    text = "id: #{@boy.id}"
-    assert_select 'p', text: text, count: 1
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@boy_most_competitions.id.to_s+"']"
+    assert_select "li[id='"+@girl_most_competitions.id.to_s+"']"
   end
 
   test "should not show incorrect dancer with most competitions" do
     get dancers_most_competitions_url
     assert_response :success
-    text = "id: #{@girl.id}"
-    assert_select 'p', text: text , count: 0
+    assert_select "li[id='"+@girl.id.to_s+"']", count: 0
   end
 end
