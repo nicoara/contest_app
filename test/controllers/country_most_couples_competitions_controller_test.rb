63f6c51462bc13e6aa6b11992dd36054aefdfcb3
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 17:31:37 +0300

    Add test for multiple solutions for country_most_couples_competitions query

--- test/controllers/country_most_couples_competitions_controller_test.rb
+++ test/controllers/country_most_couples_competitions_controller_test.rb
@@ -2,33 +2,38 @@ require 'test_helper'
 
 class CountryMostCouplesCompetitionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    @girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    girl_italy = create(:girl, name: 'Alessandra Elati', country: 'Italy')
-    couple_diff_country = create(:couple, boy_id: @boy_germany.id, girl_id: girl_italy.id)
-    couple_germany = create(:couple, boy_id: @boy_germany.id, girl_id: @girl_germany.id)
+    @boy_germany = create(:boy, country: 'Germany')
+    girl_germany = create(:girl, country: 'Germany')
+    @boy_italy = create(:boy, country: 'Italy')
+    girl_italy = create(:girl, country: 'Italy')
+    @boy_romania = create(:boy, country: 'Romania')
+    couple = create(:couple, boy_id: @boy_romania.id, girl_id: girl_italy.id)
+    couple_2 = create(:couple, boy_id: @boy_germany.id, girl_id: girl_germany.id)
+    couple_3 = create(:couple, boy_id: @boy_italy.id, girl_id: girl_italy.id)
 
     competition = create(:competition)
     division = create(:division)
     age_section = create(:age_section)
-    entry = create(:entry, couple_id: couple_diff_country.id, competition_id: competition.id,
+    create(:entry, couple_id: couple.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id,
-                      created_at: 1.years.ago )
-    entry_2 = create(:entry, couple_id: couple_germany.id, competition_id: competition.id,
+                      created_at: 1.years.ago)
+    create(:entry, couple_id: couple_2.id, competition_id: competition.id,
+                      division_id: division.id, age_section_id: age_section.id)
+    create(:entry, couple_id: couple_3.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id)
   end
 
   test "should show correct country with most couples at competitions" do
     get country_most_couples_competitions_url
     assert_response :success
-    text = "Country: Germany"
-    assert_select 'p', text: text, count: 1
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@boy_germany.country.to_s+"']"
+    assert_select "li[id='"+@boy_italy.country.to_s+"']"
   end
 
   test "should not show incorrect country with most couples at competitions" do
     get country_most_couples_competitions_url
     assert_response :success
-    text = "Country: Italy"
-    assert_select 'p', text: text, count: 0
+    assert_select "li[id='"+@boy_romania.country.to_s+"']", count: 0
   end
 end
