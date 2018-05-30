4edb54681231811a87550b9c70d98c4480a25912
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 15:44:40 +0300

    Improve performance dancers_most_competitions query

--- app/controllers/dancers_most_competitions_controller.rb
+++ app/controllers/dancers_most_competitions_controller.rb
@@ -1,29 +1,37 @@
 class DancersMostCompetitionsController < ApplicationController
   def index
-    boys = Couple
+    boy_max = Couple
       .joins(:entries)
       .group('boy_id')
       .order('total DESC')
-      .select("couples.boy_id as id, count(couples.boy_id) as total")
-    girls = Couple
+      .select("count(couples.boy_id) as total")
+      .first
+
+    girl_max = Couple
       .joins(:entries)
       .group('girl_id')
       .order('total DESC')
-      .select("couples.girl_id as id, count(couples.girl_id) as total")
+      .select("count(couples.girl_id) as total")
+      .first
 
-    @max_entries = boys.length > 0 ? boys.first.total : 0
-    @max_entries = girls.length > 0 && girls.first.total > @max_entries ? girls.first.total : @max_entries
+    @max_entries = boy_max != nil ? boy_max.total : 0
+    @max_entries = girl_max != nil && girl_max.total > @max_entries ? girl_max.total : @max_entries
 
-    @dancers_most_entries = []
-    dancers = [boys, girls]
-    dancers.each do |dancer_list|
-      dancer_list.each do |dancer|
-        if dancer.total == @max_entries
-          @dancers_most_entries.push(dancer.id)
-        else
-          break
-        end
-      end
-    end
+    boys = Couple
+      .joins(:entries)
+      .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
+      .group('boy_id')
+      .having('count(couples.boy_id) == ' + @max_entries.to_s)
+      .select("couples.boy_id as id, dancers.name as name")
+
+    girls = Couple
+      .joins(:entries)
+      .joins("LEFT JOIN dancers ON couples.girl_id = dancers.id")
+      .group('girl_id')
+      .having('count(couples.girl_id) == ' + @max_entries.to_s)
+      .select("couples.girl_id as id, dancers.name as name")
+
+    @dancers_most_entries = [boys, girls].flatten
+
   end
 end
