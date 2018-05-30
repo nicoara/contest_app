1b9da64b320418b76f002ae15ba59316e971eb2a
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 12:50:01 +0300

    Add test for multiple solutions for international_couples query

--- test/controllers/international_couples_controller_test.rb
+++ test/controllers/international_couples_controller_test.rb
@@ -2,24 +2,26 @@ require 'test_helper'
 
 class InternationalCouplesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    @girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    girl_italy = create(:girl, name: 'Alessandra Elati', country: 'Italy')
+    @boy_germany = create(:boy, country: 'Germany')
+    @girl_germany = create(:girl, country: 'Germany')
+    boy_romania = create(:boy, country:'Romania')
+    girl_italy = create(:girl, country: 'Italy')
     @couple_diff_country = create(:couple, boy_id: @boy_germany.id, girl_id: girl_italy.id)
+    @couple_diff_country_2 = create(:couple, boy_id: boy_romania.id, girl_id: girl_italy.id)
     @couple_same_country = create(:couple, boy_id: @boy_germany.id, girl_id: @girl_germany.id)
   end
 
   test "should show correct couple on query couple diff countries" do
     get international_couples_url
     assert_response :success
-    text = "id: #{@couple_diff_country.id}"
-    assert_select 'p', text
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@couple_diff_country.id.to_s+"']"
+    assert_select "li[id='"+@couple_diff_country_2.id.to_s+"']"
   end
 
   test "should not show incorrect couple on query couple diff countries" do
     get international_couples_url
     assert_response :success
-    text = "id: #{@couple_same_country.id}"
-    assert_select "p", text: text, count: 0
+    assert_select "li[id='"+@couple_same_country.id.to_s+"']", count: 0
   end
 end
