8a4e34802cbce05a8dd6b0119af442814f222492
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 13:27:33 +0300

    Add dancer names in devoted_dancers query

--- app/controllers/devoted_dancers_controller.rb
+++ app/controllers/devoted_dancers_controller.rb
@@ -1,13 +1,15 @@
 class DevotedDancersController < ApplicationController
   def index
     boys = Couple
+      .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
       .group('boy_id')
       .having('count(boy_id)=1')
-      .select('boy_id as id')
+      .select('boy_id as id, dancers.name as name')
     girls = Couple
+      .joins("LEFT JOIN dancers ON couples.girl_id = dancers.id")
       .group('girl_id')
       .having('count(girl_id)=1')
-      .select('girl_id as id')
+      .select('girl_id as id, dancers.name as name')
     @lst_dancers = [boys , girls].flatten
   end
 end
