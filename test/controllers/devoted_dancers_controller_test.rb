233b12673a49c7e8e3db18e23d285d8268e3cb95
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 13:32:31 +0300

    Add test for multiple solutions for devoted_dancers query

--- test/controllers/devoted_dancers_controller_test.rb
+++ test/controllers/devoted_dancers_controller_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class DevotedDancersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    @girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    girl_italy = create(:girl, name: 'Alessandra Elati', country: 'Italy')
-    @couple_diff_country = create(:couple, boy_id: @boy_germany.id, girl_id: girl_italy.id)
-    @couple_same_country = create(:couple, boy_id: @boy_germany.id, girl_id: @girl_germany.id)
+    @boy = create(:boy)
+    @girl_devoted = create(:girl)
+    @girl_devoted_2 = create(:girl)
+    create(:couple, boy_id: @boy.id, girl_id: @girl_devoted_2.id)
+    create(:couple, boy_id: @boy.id, girl_id: @girl_devoted.id)
   end
 
   test "should show correct dancer on query dancer in one couple" do
     get devoted_dancers_url
     assert_response :success
-    text = "id: #{@girl_germany.id}"
-    assert_select 'p', text
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@girl_devoted.id.to_s+"']"
+    assert_select "li[id='"+@girl_devoted_2.id.to_s+"']"
   end
 
   test "should not show incorrect dancer on query dancer in one couple" do
     get devoted_dancers_url
     assert_response :success
-    text = "id: #{@boy_germany.id}"
-    assert_select 'p', text: text, count: 0
+    assert_select "li[id='"+@boy.id.to_s+"']", count: 0
   end
 end
