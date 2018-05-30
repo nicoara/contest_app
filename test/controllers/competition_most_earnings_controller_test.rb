fd634367a74df9f4a1160caf27434c484d61b6c3
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 16:58:59 +0300

    Add test for multiple solutions for competition_most_earnings query

--- test/controllers/competition_most_earnings_controller_test.rb
+++ test/controllers/competition_most_earnings_controller_test.rb
@@ -2,31 +2,34 @@ require 'test_helper'
 
 class CompetitionMostEarningsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    couple_germany = create(:couple, boy_id: boy_germany.id, girl_id: girl_germany.id)
+    boy = create(:boy)
+    girl = create(:girl)
+    couple = create(:couple, boy_id: boy.id, girl_id: girl.id)
 
     @competition_cheap = create(:competition, price: 100)
     @competition_expensive = create(:competition, price: 200)
+    @competition_expensive_2 = create(:competition, price: 200)
     division = create(:division)
     age_section = create(:age_section)
-    entry = create(:entry, couple_id: couple_germany.id, competition_id: @competition_cheap.id,
+    create(:entry, couple_id: couple.id, competition_id: @competition_cheap.id,
                       division_id: division.id, age_section_id: age_section.id)
-    entry_2 = create(:entry, couple_id: couple_germany.id, competition_id: @competition_expensive.id,
+    create(:entry, couple_id: couple.id, competition_id: @competition_expensive.id,
+                      division_id: division.id, age_section_id: age_section.id)
+    create(:entry, couple_id: couple.id, competition_id: @competition_expensive_2.id,
                       division_id: division.id, age_section_id: age_section.id)
   end
 
   test "should show correct competition with most earnings" do
     get competition_most_earnings_url
     assert_response :success
-    text = "Competition id: #{@competition_expensive.id}"
-    assert_select 'p', text: text, count: 1
+    assert_select 'li', count: 2
+    assert_select "li[id='"+@competition_expensive.id.to_s+"']"
+    assert_select "li[id='"+@competition_expensive_2.id.to_s+"']"
   end
 
   test "should not show incorrect competition with most earnings" do
     get competition_most_earnings_url
     assert_response :success
-    text = "Competition id: #{@competition_cheap.id}"
-    assert_select 'p', text: text, count: 0
+    assert_select "li[id='"+@competition_cheap.id.to_s+"']", count: 0
   end
 end
