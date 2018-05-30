81dc727eb1aa0a0c6244c4ad99c56b1070c30c7c
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 16:09:15 +0300

    Add dancer names in dancers_most_divisions query

--- app/controllers/dancers_most_divisions_controller.rb
+++ app/controllers/dancers_most_divisions_controller.rb
@@ -2,14 +2,16 @@ class DancersMostDivisionsController < ApplicationController
   def index
     boys = Couple
       .joins(:entries)
+      .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
       .group('boy_id')
       .order('total DESC')
-      .select("couples.boy_id as dancer_id, count(distinct(entries.division_id)) as total")
+      .select("couples.boy_id as id, count(distinct(entries.division_id)) as total, dancers.name as name")
     girls = Couple
       .joins(:entries)
+      .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
       .group('girl_id')
       .order('total DESC')
-      .select("couples.girl_id as dancer_id, count(distinct(entries.division_id)) as total")
+      .select("couples.girl_id as id, count(distinct(entries.division_id)) as total, dancers.name as name")
 
     @max_divisions = boys.length > 0 ? boys.first.total : 0
     @max_divisions = girls.length > 0 && girls.first.total > @max_divisions ? girls.first.total : @max_divisions
@@ -19,7 +21,7 @@ class DancersMostDivisionsController < ApplicationController
     dancers.each do |dancer_list|
       dancer_list.each do |dancer|
         if dancer.total == @max_divisions
-          @dancers_most_divisions.push(dancer.dancer_id)
+          @dancers_most_divisions.push(dancer)
         else
           break
         end
