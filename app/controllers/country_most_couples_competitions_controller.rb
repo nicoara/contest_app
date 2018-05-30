b0ac90d9fb32299f8e524a669081ebe6a53d2f5e
Nicoara <nicoaratalpes@gmail.com>
2018-05-30 17:23:32 +0300

    Add multiple solutions for country_most_couples_competitions

--- app/controllers/country_most_couples_competitions_controller.rb
+++ app/controllers/country_most_couples_competitions_controller.rb
@@ -1,17 +1,27 @@
 class CountryMostCouplesCompetitionsController < ApplicationController
   def index
-    country_most_couples_competitions = Entry
+    most_couples_competitions = Entry
     .joins(:couple)
     .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
     .joins("LEFT JOIN dancers as d1 ON couples.girl_id = d1.id")
     .where('dancers.country = d1.country')
-    .group('dancers.country').order('total DESC')
-    .select("dancers.country as country, count(entries.id) as total")
+    .group('dancers.country')
+    .order('total DESC')
+    .select("count(entries.id) as total")
     .first
 
-    if country_most_couples_competitions != nil
-      @country = country_most_couples_competitions.country
-      @nr_dancers = country_most_couples_competitions.total
+    if most_couples_competitions != nil
+      @nr_dancers = most_couples_competitions.total
+
+      @countries_most_couples_competitions = Entry
+        .joins(:couple)
+        .joins("LEFT JOIN dancers ON couples.boy_id = dancers.id")
+        .joins("LEFT JOIN dancers as d1 ON couples.girl_id = d1.id")
+        .where('dancers.country = d1.country')
+        .group('dancers.country')
+        .having('count(entries.id) == ' + @nr_dancers.to_s)
+        .select("dancers.country as name")
+
     end
   end
 end
