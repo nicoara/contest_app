a867c209bf29349ad9970cfd22eadc9e0df7119a
Nicoara <nicoaratalpes@gmail.com>
2018-05-31 19:46:04 +0300

    Bugfix - rewrite dancer_slowest_divisions_controller to account for division involution

--- app/controllers/dancer_slowest_divisions_controller.rb
+++ app/controllers/dancer_slowest_divisions_controller.rb
@@ -1,15 +1,15 @@
 class DancerSlowestDivisionsController < ApplicationController
-  def index
-    #mod de calcul:
-    #DX: data la care un dansator a participat prima oară la categoria X
-    #viteza:
-    # pentru dasatorii care au participat la o singură categorie, este infinit
-    # pentru dansatorii care au participat la mai multe categorii:
-    # V = (Y-X)/(DY-DX)
-    # unde
-    # * X e categoria cea mai slabă la care a participat vreodată
-    # * Y e categoria cea mai tare la care a participat vreodată
 
+  #mod de calcul:
+  #DX: data la care un dansator a participat prima oară la categoria X
+  #viteza:
+  # pentru dasatorii care au participat la o singură categorie, este infinit
+  # pentru dansatorii care au participat la mai multe categorii:
+  # V = (Y-X)/(DY-DX)
+  # unde
+  # * X e categoria cea mai slabă la care a participat vreodată
+  # * Y e categoria cea mai tare la care a participat vreodată
+  def index
     boy_entries = Couple
       .joins(:entries)
       .select("couples.boy_id as dancer_id, entries.division_id as division_id, entries.created_at")
@@ -17,6 +17,7 @@ class DancerSlowestDivisionsController < ApplicationController
       .joins(:entries)
       .select("couples.girl_id as dancer_id, entries.division_id as division_id, entries.created_at")
 
+    dancer_multiple_divisions = false
     dancer_division_dates = {}
     entries = [boy_entries, girl_entries]
     entries.each do |entries_gender|
@@ -28,6 +29,9 @@ class DancerSlowestDivisionsController < ApplicationController
             end
           else
             dancer_division_dates[entry.dancer_id][entry.division_id] = entry.created_at
+            if dancer_multiple_divisions == false && dancer_division_dates[entry.dancer_id].keys.length > 1
+              dancer_multiple_divisions = true
+            end
           end
         else
           dancer_division_dates[entry.dancer_id] = { entry.division_id => entry.created_at}
@@ -35,35 +39,41 @@ class DancerSlowestDivisionsController < ApplicationController
       end
     end
 
-    slowest_dancer_ids = []
-    slowest_time = nil
-    dancer_division_dates.each do |dancer_id, division_dates|
-      dancer_time = dancer_time(division_dates)
-
-      if slowest_time == nil
-        slowest_time = dancer_time
-        slowest_dancer_ids = [dancer_id]
-      else
-        if dancer_time < slowest_time
-          slowest_time = dancer_time
-          slowest_dancer_ids = [dancer_id]
-        elsif dancer_time == slowest_time
-          slowest_dancer_ids.push(dancer_id)
-        end
-      end
-    end
-
-    @slowest_dancers = Dancer.where(id: slowest_dancer_ids)
+    @slowest_dancers = Dancer.where(id: slowest_dancer_ids(dancer_division_dates, dancer_multiple_divisions))
   end
 
   private
-    def dancer_time(division_dates)
-      if division_dates.keys.count == 1
-        return 0 #if dancer has participated in just one division, time is infinite
-      else
-        return (division_dates.keys.max - division_dates.keys.min) /
-          (division_dates[division_dates.keys.max] - division_dates[division_dates.keys.min])
+    def slowest_dancer_ids(dancer_division_dates, dancer_multiple_divisions)
+      if dancer_multiple_divisions == false
+        return dancer_division_dates.keys
       end
+
+      slowest_dancer_ids = []
+      slowest_time = nil
+      dancer_division_dates.each do |dancer_id, division_dates|
+        if division_dates.keys.length > 1
+          dancer_time = dancer_time(division_dates)
+          if dancer_time > 0 # <0 means a dancer went down in division strength in time
+            if slowest_time == nil
+              slowest_time = dancer_time
+              slowest_dancer_ids = [dancer_id]
+            else
+              if dancer_time < slowest_time
+                slowest_time = dancer_time
+                slowest_dancer_ids = [dancer_id]
+              elsif dancer_time == slowest_time
+                slowest_dancer_ids.push(dancer_id)
+              end
+            end
+          end
+        end
+      end
+      puts '------slowest_dancer_ids', slowest_dancer_ids
+      return slowest_dancer_ids
     end
 
+    def dancer_time(division_dates)
+      return (division_dates.keys.max - division_dates.keys.min) /
+        (division_dates[division_dates.keys.max] - division_dates[division_dates.keys.min])
+    end
 end
