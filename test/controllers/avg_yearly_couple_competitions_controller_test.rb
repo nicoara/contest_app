c0ef6be9df99284e4bd03e71c9e938028c5e945b
Nicoara <nicoaratalpes@gmail.com>
2018-05-31 12:58:31 +0300

    Add test for multiple solutions for avg_yearly_couple_competitions query

--- test/controllers/avg_yearly_couple_competitions_controller_test.rb
+++ test/controllers/avg_yearly_couple_competitions_controller_test.rb
@@ -2,26 +2,35 @@ require 'test_helper'
 
 class AvgYearlyCoupleCompetitionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @boy_germany = create(:boy, name: 'Jurgen Klopp', country: 'Germany')
-    @girl_germany = create(:girl, name: 'Angela Merkel', country: 'Germany')
-    girl_italy = create(:girl, name: 'Alessandra Elati', country: 'Italy')
-    couple_diff_country = create(:couple, boy_id: @boy_germany.id, girl_id: girl_italy.id)
-    couple_same_country = create(:couple, boy_id: @boy_germany.id, girl_id: @girl_germany.id)
+    @boy = create(:boy)
+    @girl = create(:girl)
+    girl_2 = create(:girl)
+    @couple = create(:couple, boy_id: @boy.id, girl_id: girl_2.id)
+    @couple_2 = create(:couple, boy_id: @boy.id, girl_id: @girl.id)
 
     competition = create(:competition)
+    competition_2 = create(:competition)
     division = create(:division)
     age_section = create(:age_section)
-    entry = create(:entry, couple_id: couple_diff_country.id, competition_id: competition.id,
+    create(:entry, couple_id: @couple.id, competition_id: competition.id,
+                      division_id: division.id, age_section_id: age_section.id)
+    create(:entry, couple_id: @couple.id, competition_id: competition_2.id,
                       division_id: division.id, age_section_id: age_section.id,
                       created_at: 1.years.ago )
-    entry_2 = create(:entry, couple_id: couple_same_country.id, competition_id: competition.id,
+    create(:entry, couple_id: @couple_2.id, competition_id: competition.id,
                       division_id: division.id, age_section_id: age_section.id)
+    create(:entry, couple_id: @couple_2.id, competition_id: competition_2.id,
+                      division_id: division.id, age_section_id: age_section.id,
+                      created_at: 4.years.ago )
   end
 
   test "should show correct avg yearly competitions" do
     get avg_yearly_couple_competitions_url
     assert_response :success
-    text = "id: #{@boy_germany.id}, avg: 1.0"
-    assert_select 'p', text: text, count: 1
+    assert_select 'li', count: 2
+    avg_yearly_couple_1 = '%.4f' % (2/3.0)
+    assert_select "li[id='"+@couple.id.to_s+"-"+avg_yearly_couple_1.to_s+"']"
+    avg_yearly_couple_2 = '%.4f' % (1/3.0)
+    assert_select "li[id='"+@couple_2.id.to_s+"-"+avg_yearly_couple_2.to_s+"']"
   end
 end
