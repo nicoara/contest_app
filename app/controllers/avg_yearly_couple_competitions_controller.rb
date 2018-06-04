cbc5493eaa01ad7788f5337af869b4ca76ac3455
Nicoara <nicoaratalpes@gmail.com>
2018-06-04 11:34:28 +0300

    Update - refactor avg_yearly_couple_competitions to one optimised sql query

--- app/controllers/avg_yearly_couple_competitions_controller.rb
+++ app/controllers/avg_yearly_couple_competitions_controller.rb
@@ -1,16 +1,11 @@
 class AvgYearlyCoupleCompetitionsController < ApplicationController
   def index
-    @couple_dancers = Couple
+    @couple_avg_yearly_competitions = Entry
+      .joins(:couple)
       .joins("LEFT JOIN dancers as d ON couples.boy_id = d.id")
       .joins("LEFT JOIN dancers as d1 ON couples.girl_id = d1.id")
-      .select('couples.id as id, d.name as boy_name, d.id as boy_id, d1.name as girl_name, d1.id as girl_id')
-
-    @couple_avg_yearly_competitions = {}
-    Entry.all.each do |entry|
-      if entry.created_at >= 36.months.ago
-        @couple_avg_yearly_competitions[entry.couple_id] = @couple_avg_yearly_competitions.key?(entry.couple_id) ?
-          @couple_avg_yearly_competitions[entry.couple_id] + 1 : 1
-      end
-    end
+      .where('entries.created_at >= ?', 36.months.ago)
+      .group(:couple_id)
+      .select('entries.couple_id as id, count(entries.couple_id) as total, d.name as boy_name, d.id as boy_id, d1.name as girl_name, d1.id as girl_id')
   end
 end
